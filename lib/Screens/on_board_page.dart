669e625e8c7ded14b92c1103412638668f8d6093
Muhareb1 <110301648+Muhareb1@users.noreply.github.com>
2023-07-30 20:22:30 +0300

    publish the carousel pages

--- lib/Screens/on_board_page.dart
+++ lib/Screens/on_board_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class OnBoardPage extends StatefulWidget {
   OnBoardPage({super.key});
@@ -12,33 +13,61 @@ class _OnBoardPageState extends State<OnBoardPage> {
 
   @override
   Widget build(BuildContext context) {
+    final controller = PageController(viewportFraction: 0.8, keepPage: true);
+    final pages = List.generate(
+        3,
+        (index) => Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                color: Colors.grey.shade300,
+              ),
+              margin: EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+              child: Container(
+                height: 280,
+                child: Center(
+                    child: Text(
+                  "Page $index",
+                  style: TextStyle(color: Colors.indigo),
+                )),
+              ),
+            ));
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Row(
-          children: [
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 30),
-              height: 10,
-              width: 60,
-              decoration:
-                  BoxDecoration(border: Border.all(color: Colors.black)),
-            ),
-            Container(
-              height: 10,
-              width: 60,
-              decoration:
-                  BoxDecoration(border: Border.all(color: Colors.black)),
-            ),
-            Container(
-              height: 10,
-              width: 60,
-              decoration:
-                  BoxDecoration(border: Border.all(color: Colors.black)),
-            ),
-          ],
-        )
-      ]),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: 16),
+              SizedBox(
+                height: 240,
+                child: PageView.builder(
+                  controller: controller,
+                  // itemCount: pages.length,
+                  itemBuilder: (_, index) {
+                    return pages[index % pages.length];
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 24, bottom: 12),
+                child: Text(
+                  'Worm',
+                  style: TextStyle(color: Colors.black54),
+                ),
+              ),
+              SmoothPageIndicator(
+                controller: controller,
+                count: pages.length,
+                effect: const WormEffect(
+                  dotHeight: 16,
+                  dotWidth: 80,
+                  type: WormType.normal,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
